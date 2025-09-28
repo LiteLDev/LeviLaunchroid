67f2b69080eba889dfc4ab47685b260189da35db
RadiantByte <radiantbyte@outlook.com>
2025-09-28 13:01:14 -0700

    fix: add missing java files

---
+++ app/src/main/java/org/levimc/launcher/core/minecraft/mcpelauncher/activities/ComposeMainActivity.kt
@@ -0,0 +1,43 @@
+package org.levimc.launcher.core.minecraft.mcpelauncher.activities
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+/**
+ * @author <a href="https://github.com/RadiantByte">RadiantByte</a>
+ */
+
+class ComposeMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val controller = WindowCompat.getInsetsController(window, window.decorView)
+                controller.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                controller.hide(WindowInsetsCompat.Type.navigationBars())
+                controller.hide(WindowInsetsCompat.Type.statusBars())
+            } else {
+                @Suppress("DEPRECATION")
+                window.decorView.systemUiVisibility = (
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/levimc/launcher/core/minecraft/mcpelauncher/activities/ComposePreloadActivity.kt
@@ -0,0 +1,72 @@
+package org.levimc.launcher.core.minecraft.mcpelauncher.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import androidx.activity.ComponentActivity
+import org.levimc.launcher.core.minecraft.mcpelauncher.ModdedPEApplication
+import org.levimc.launcher.core.minecraft.mcpelauncher.data.Preferences
+import org.levimc.launcher.core.minecraft.pesdk.PreloadException
+import org.levimc.launcher.core.minecraft.pesdk.Preloader
+
+
+/**
+ * @author <a href="https://github.com/RadiantByte">RadiantByte</a>
+ */
+
+class ComposePreloadActivity : ComponentActivity() {
+    private companion object {
+        const val MSG_START_MINECRAFT = 1
+        const val MSG_ERROR = 3
+    }
+
+    private val preloadUIHandler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG_START_MINECRAFT -> {
+                    val intent = Intent(this@ComposePreloadActivity, MinecraftActivity::class.java)
+                    intent.putExtras(msg.data)
+                    startActivity(intent)
+                    finish()
+                }
+
+                MSG_ERROR -> {
+                    val preloadException = msg.obj as PreloadException
+                    Preferences.openGameFailed = preloadException.toString()
+                    finish()
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        PreloadThread().start()
+    }
+
+    private inner class PreloadThread : Thread() {
+        override fun run() {
+            try {
+                val preloader = Preloader(
+                    ModdedPEApplication.mPESdk,
+                    null,
+                    object : Preloader.PreloadListener() {
+                        override fun onFinish(bundle: Bundle) {
+                            val message = Message()
+                            message.what = MSG_START_MINECRAFT
+                            message.data = bundle
+                            preloadUIHandler.sendMessage(message)
+                        }
+                    })
+                preloader.preload(this@ComposePreloadActivity)
+            } catch (e: PreloadException) {
+                val message = Message()
+                message.what = MSG_ERROR
+                message.obj = e
+                preloadUIHandler.sendMessage(message)
+            }
+        }
+    }
+}
