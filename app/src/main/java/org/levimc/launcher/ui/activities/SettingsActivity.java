5f79554d31c33dd4b9abfca771e02d43a6066091
Katilmamiofficial <akbalmuhammed668@gmail.com>
2026-06-12 02:53:41 +0300

    Add Turkish language support in SettingsActivity

--- app/src/main/java/org/levimc/launcher/ui/activities/SettingsActivity.java
+++ app/src/main/java/org/levimc/launcher/ui/activities/SettingsActivity.java
@@ -193,7 +193,8 @@ public class SettingsActivity extends BaseActivity {
                 getString(R.string.portuguese),
                 getString(R.string.french),
                 getString(R.string.japanese),
-                getString(R.string.hindi)
+                getString(R.string.hindi),
+                getString(R.string.turkish)
         };
 
         String currentCode = languageManager.getCurrentLanguage();
@@ -206,6 +207,7 @@ public class SettingsActivity extends BaseActivity {
             case "fr" -> 6;
             case "ja" -> 7;
             case "hi" -> 8;
+            case "tr" -> 9;
             default -> 0;
         };
 
@@ -230,6 +232,7 @@ public class SettingsActivity extends BaseActivity {
                     case 6 -> "fr";
                     case 7 -> "ja";
                     case 8 -> "hi";
+                    case 9 -> "tr";
                     default -> "en";
                 };
                 if (!code.equals(languageManager.getCurrentLanguage())) {
